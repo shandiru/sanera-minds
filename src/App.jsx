991befde70fbad8ad825ceaef482cb87f0fcb750
shandiru <msddiru@gmail.com>
2025-11-19 19:03:54 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Homepage from './Page/Homepage'; // Use relative path correctly
 import Aboutpage from './Page/Aboutpage'; // Use relative path correctly
-import WorkshopsPage from './Page/Workshoppage';
+import Eventspage from './Page/Eventspage';
 import EducationPage from "./Page/EducationPage"
 import OrganisationPage from "./Page/OrganisationPage"
 import BookNowPage from "./Page/BookNowPage"
@@ -14,7 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
          <Route path="/about" element={<Aboutpage />} />
-          <Route path="/services/events" element={<WorkshopsPage />} />
+          <Route path="/services/events" element={<Eventspage />} />
           <Route path="/services/education" element={<EducationPage />} />
            <Route path="/services/organisation" element={<OrganisationPage />} />
              <Route path="/contact" element={<BookNowPage />} />
